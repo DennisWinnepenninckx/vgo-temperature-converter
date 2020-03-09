a68e060a13f496ce83fc18f9fc870ae690d252af
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2020-03-09 21:20:10 +0100

    Kelvin done

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -23,11 +23,28 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+
         }
-    
-        private void ConvertToCelsius(object sender, RoutedEventArgs e)
+
+        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
         {
-            textBox.Text = "You clicked the button";
+            var doubleFaren = double.Parse(FahrenheitBox.Text);
+            var celsius = (doubleFaren - 32) / 1.8;
+            var kelvin = celsius + 273.15;
+            CelsiusBox.Text = celsius.ToString();
+            KelvinBox.Text = kelvin.ToString();
+        }
+        private void ConvertCelsius(object sender, RoutedEventArgs e)
+        {
+            var doubleCelsius = double.Parse(CelsiusBox.Text);
+            var faren = (doubleCelsius * 1.8) + 32;
+            FahrenheitBox.Text = faren.ToString();
+            KelvinBox.Text = (doubleCelsius+273.15).ToString();
+        }
+        private void ConvertKelvin(object sender, RoutedEventArgs e) {
+            var kelvin = double.Parse(KelvinBox.Text);
+            CelsiusBox.Text = (kelvin - 273.15).ToString();
+            FahrenheitBox.Text = ((kelvin - 273.15) * 1.8 + 32).ToString();
         }
     }
 }
