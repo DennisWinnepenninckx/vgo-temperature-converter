ed431efbac969e62345d793b611a42ba89acafbb
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2020-03-14 10:09:39 +0100

    added Celsiusconverter

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -27,13 +27,6 @@ namespace View
 
         }
 
-        private void SliderValueChanged(object sender, RoutedEventArgs e)
-        {
-            var sliderValue = slider.Value;
-
-            CelsiusBox.Text = (sliderValue - 273.15).ToString();
-            FahrenheitBox.Text = ((sliderValue - 273.15)*1.8 + 32).ToString();
-        }
     }
     public class CelsiusConverter : IValueConverter
     {
@@ -51,6 +44,24 @@ namespace View
             var kelvin = celsius + 273.15;
 
             return kelvin;
+        }
+    }
+    public class FahrenheitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var Fahrenheit = (kelvin - 273.15) * 1.8 + 32;
+
+            return Fahrenheit.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var Fahrenheit = (celsius + 273.15)/1.8 - 32;
+
+            return Fahrenheit;
         }
     }
 }
