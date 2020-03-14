ee587c0893901654b5bbf1e02c230ce0b5c875f9
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2020-03-14 10:15:58 +0100

    added fahrenheit converter

--- TemperatureConverter/View/TemperatureScale.cs
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace View
 {
-    class Class1
+    public interface ITemperatureScale
     {
+        string Name { get; }
+
+        double ConvertToKelvin(double temperature);
+
+        double ConvertFromKelvin(double temperature);
+    }
+
+    public class KelvinTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Kelvin";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature;
+        }
+    }
+
+    public class CelsiusTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Celsius";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            return temperature - 273.15;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return temperature + 273.15;
+        }
+    }
+    public class FahrenheitTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Fahrenheit";
+        public double ConvertFromKelvin(double temperature)
+        {
+            return (temperature - 273.15)/1.8 - 32;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            return (temperature + 273.15) * 1.8 + 32;
+        }
     }
 }
