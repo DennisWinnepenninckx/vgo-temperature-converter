8cb5a958580a5d201e37f0fac262136aacb7a909
Dezdekke <dennis.winnepenninckx@student.ucll.be>
2020-03-22 21:41:10 +0100

    PreItemControl

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -18,9 +18,6 @@ using System.Windows.Shapes;
 
 namespace View
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,25 +25,6 @@ namespace View
             InitializeComponent();
 
             this.DataContext = new ConverterViewModel();
-        }
-    }
-
-    public class TemperatureConverter : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-
-            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temperature = double.Parse((string)value);
-
-            return this.TemperatureScale.ConvertToKelvin(temperature);
         }
     }
 }
